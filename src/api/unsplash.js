14eb666932c2b5ba77d20f14c416c65e23fa0f55
RizwanSAA <sabaarizwan@outlook.com>
2020-09-18 22:55:24 +0530

    removed keys

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const unsplash = axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID MSMMhA5pfboOlWFHTxWCQF5XoMGqT7_dyuIzLumbDNA",
+    Authorization: "Client-ID paste you key",
   },
 });
 
